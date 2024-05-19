ebe21021a9535f916e2bbedb9c616ff02585c3b1
Amro Sati <amrosatti98@gmail.com>
2024-05-19 15:03:39 +0300

    test module for BaseMode\ has 2 errors and 2 false could not figure

---
+++ tests/test_basemodel.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python3
+
+"""Unittest for `BaseModel` Class
+"""
+
+from models.base_model import BaseModel
+from datetime import datetime
+from uuid import uuid4
+import unittest
+import re
+
+class TestBaseModel(unittest.TestCase):
+    """Tests `BaseModel` Class
+    """
+
+    def setUp(self):
+        """Instantiates a `BaseModel` Object
+        """
+        self.test_id = str(uuid4())
+        self.test_createdat = datetime(2024, 1, 1, 00, 00, 00, 00).isoformat()
+        self.test_updatedat = datetime.now().isoformat()
+        self.create_time = datetime.now()
+        self.test_obj1 = BaseModel()
+        self.test_obj2 = BaseModel({
+                    'id': self.test_id,
+                    'created_at': self.test_createdat,
+                    'updated_at': self.test_updatedat,
+                    'something': 'something_else'
+                })
+
+    def test_init(self):
+        """Tests the initialization of instances
+        """
+        '''Empty Arguments'''
+        self.assertTrue('id' in dir(self.test_obj1))
+        self.assertTrue('created_at' in dir(self.test_obj1))
+        self.assertTrue('updated_at' in dir(self.test_obj1))
+
+        '''Valid kwargs'''
+        self.assertTrue('id' in dir(self.test_obj2))
+        self.assertTrue('created_at' in dir(self.test_obj2))
+        self.assertTrue('updated_at' in dir(self.test_obj2))
+        self.assertTrue('something' in dir(self.test_obj2))
+
+    def test_attributes(self):
+        """Tests the instance attributes
+        """
+        '''id'''
+        self.assertTrue(isinstance(self.test_obj1.id, str))
+        self.assertEqual(self.test_obj2.id, str(self.test_id))
+
+        '''created_at'''
+        self.assertTrue(isinstance(self.test_obj1.created_at), datetime)
+        self.assertEqual(isinstance(self.test_obj2.created_at, datetime))
+        self.assertTrue(self.test_obj1.created_at is self.create_time)
+        self.assertEqual(self.test_obj1.created_at, self.create_time)
+        self.assertEqual(
+                    self.test_obj2.created_at,
+                    datetime.fromisoformat('2024-01-01T00:00:00.00')
+                )
+
+        '''updated_at'''
+        self.assertTrue(isinstance(self.test_obj1.updated_at, datetime))
+        self.assertTrue(isinstance(self.test_obj2.updated_at, datetime))
+        self.assertEqual(self.test_obj1.created_at, self.test_obj1.updated_at)
+        self.assertEqual(self.test_obj2.updated_at, self.create_time)
+
+    def test_save(self):
+        """Tests `save()` instance method
+        """
+        current_time = datetime.now()
+        self.test_obj1.save()
+        self.test_obj2.save()
+        self.assertEqual(current_time, self.test_obj1.updated_at)
+        self.assertEqual(current_time, self.test_obj2.updated_at)
+
+    def test_to_dict(self):
+        """Tests `to_dict()` instance method
+        """
+        self.assertTrue(isinstance(self.test_obj1.to_dict(), dict))
+        self.assertTrue(isinstance(self.test_obj2.to_dict(), dict))
+        for k in list(self.test_obj1.to_dict().keys()):
+            if k != '__class__':
+                self.assertTrue(k in list(self.test_obj1.__dict__.keys()))
+
+        for k in list(self.test_obj2.to_dict().keys()):
+            if k != '__class__':
+                self.assertTrue(k in list(self.test_obj2.__dict__.keys()))
+
+        self.assertTrue('__class__' in list(self.test_obj1.to_dict().keys()))
+        self.assertTrue('__class__' in list(self.test_obj2.to_dict().keys()))
+        self.assertEqual(BaseModel, self.test_obj1.to_dict()['__class__'])
+        self.assertEqual(BaseModel, self.test_obj2.to_dict()['__class__'])
+
+        self.assertTrue(isinstance(self.test_obj1.to_dict()['created_at'], str))
+        self.assertEqual(
+                    re.findall(
+                            '\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{2}',
+                            self.test_obj1.to_dict()['created_at']
+                        ),
+                    []
+                )
+        self.assertTrue(isinstance(self.test_obj1.to_dict()['updated_at'], str))
+        self.assertEqual(
+                    re.findall(
+                            '\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{2}',
+                            self.test_obj1.to_dict()['updated_at']
+                        ),
+                    []
+                )
+        self.assertTrue(isinstance(self.test_obj2.to_dict()['created_at'], str))
+        self.assertEqual(
+                    re.findall(
+                            '\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{2}',
+                            self.test_obj2.to_dict()['created_at']
+                        ),
+                    []
+                )
+        self.assertTrue(isinstance(self.test_obj2.to_dict()['updated_at'], str))
+        self.assertEqual(
+                    re.findall(
+                            '\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{2}',
+                            self.test_obj2.to_dict()['updated_at']
+                        ),
+                    []
+                )
+
+if __name__ == '__main__':
+    unittest.main()
