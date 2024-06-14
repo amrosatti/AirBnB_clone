d01293bec257c25925f4da1b6ff3c923cb0cbe8c
Amro Sati <amrosatti98@gmail.com>
2024-06-14 21:25:49 +0300

    finalized the test suiet of the base model\ refactored test_basemodel.py\ fixed basemodel and filestorage classes issues

--- tests/test_basemodel.py
+++ tests/test_basemodel.py
@@ -9,155 +9,95 @@ from uuid import uuid4
 import unittest
 import re
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> fileStorage_engine
-=======
-
->>>>>>> test_models
 class TestBaseModel(unittest.TestCase):
     """Tests `BaseModel` Class
     """
+
+    p = r"[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}"
 
     def setUp(self):
         """Instantiates a `BaseModel` Object
         """
         self.test_id = str(uuid4())
-        self.test_createdat = datetime(
-                2024, 1, 1,
-                00, 00, 00, 00).isoformat()
+        self.test_createdat = datetime(2024, 1, 1, 00, 00, 00, 00).isoformat()
         self.test_updatedat = datetime.now().isoformat()
+        self.create_time = datetime.now()
         self.test_obj1 = BaseModel()
-        self.test_obj2 = BaseModel(
-                    id=self.test_id, created_at=self.test_createdat,
-                    updated_at=self.test_updatedat, something='something_else'
-                )
+        self.test_obj2 = BaseModel(**{
+                    'id': self.test_id,
+                    'created_at': self.test_createdat,
+                    'updated_at': self.test_updatedat,
+                    'something': 'something_else'
+                })
+        self.objects = [self.test_obj1, self.test_obj2]
 
     def test_init(self):
         """Tests the initialization of instances
         """
-        self.assertTrue('id' in dir(self.test_obj1))
-        self.assertTrue('created_at' in dir(self.test_obj1))
-        self.assertTrue('updated_at' in dir(self.test_obj1))
+        '''Empty Arguments'''
+        for test_object in self.objects:
+            self.assertTrue('id' in dir(test_object))
+            self.assertTrue('created_at' in dir(test_object))
+            self.assertTrue('updated_at' in dir(test_object))
+            if test_object.id == self.test_obj2.id:
+                self.assertTrue('something' in dir(test_object))
 
-    def test_init_kwargs(self):
-        """Valid kwargs
+    def test_attributes(self):
+        """Tests the instance attributes
         """
-        self.assertTrue('id' in dir(self.test_obj2))
-        self.assertTrue('created_at' in dir(self.test_obj2))
-        self.assertTrue('updated_at' in dir(self.test_obj2))
-        self.assertTrue('something' in dir(self.test_obj2))
+        uuid4_pattern = re.compile(self.p)
 
-    def test_attributes_id(self):
-        """Tests the instance attribute `id`
-        """
-        self.assertTrue(isinstance(self.test_obj1.id, str))
-        self.assertTrue(isinstance(self.test_obj2.id, str))
+        for test_object in self.objects:
+            '''id'''
+            self.assertTrue(isinstance(test_object.id, str))
+            self.assertTrue(uuid4_pattern.match(test_object.id))
 
-    def test_attributes_created_at(self):
-        """Tests `created_at` attribute
-        """
-        self.assertTrue(isinstance(self.test_obj1.created_at, datetime))
-        self.assertTrue(isinstance(self.test_obj2.created_at, datetime))
-        self.assertTrue(self.test_obj1.created_at is self.create_time)
-        self.assertEqual(self.test_obj1.created_at, self.create_time)
-        self.assertEqual(
-                    self.test_obj2.created_at,
-                    datetime.fromisoformat('2024-01-01T00:00:00')
-                )
+            '''created_at'''
+            self.assertTrue(isinstance(test_object.created_at, datetime))
+            self.assertTrue(isinstance(test_object.created_at, datetime))
+            if test_object.created_at == self.test_obj2.created_at:
+                self.assertEqual(
+                            self.test_obj2.created_at,
+                            datetime.fromisoformat('2024-01-01T00:00:00')
+                        )
 
-    def test_attributes_updated_at(self):
-        """Tests `updated_at` attribute
-        """
-        self.assertTrue(isinstance(self.test_obj1.updated_at, datetime))
-        self.assertTrue(isinstance(self.test_obj2.updated_at, datetime))
-        self.assertEqual(self.test_obj1.created_at, self.test_obj1.updated_at)
+            '''updated_at'''
+            self.assertTrue(isinstance(test_object.updated_at, datetime))
+            if test_object.updated_at == self.test_obj1.updated_at:
+                self.assertEqual(
+                        test_object.created_at,
+                        test_object.updated_at
+                        )
 
     def test_save(self):
         """Tests `save()` instance method
         """
-        test_update1 = self.test_obj1.updated_at
-        test_update2 = self.test_obj2.updated_at
-        self.test_obj1.save()
-        self.test_obj2.save()
-        self.assertTrue(test_update1 != self.test_obj1.updated_at)
-        self.assertTrue(test_update2 != self.test_obj2.updated_at)
+        for test_object in self.objects:
+            old_update = test_object.updated_at
+            test_object.save()
+            self.assertTrue(old_update != test_object.updated_at)
 
     def test_to_dict(self):
         """Tests `to_dict()` instance method
         """
-        dt_pattern = r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.?\d*'
-        self.assertTrue(isinstance(self.test_obj1.to_dict(), dict))
-        self.assertTrue(isinstance(self.test_obj2.to_dict(), dict))
-        for k in list(self.test_obj1.to_dict().keys()):
-            if k != '__class__':
-                self.assertTrue(k in list(self.test_obj1.__dict__.keys()))
+        pattern = r"\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d"
+        iso_pattern = re.compile(pattern)
+        cls = '__class__'
 
-        for k in list(self.test_obj2.to_dict().keys()):
-            if k != '__class__':
-                self.assertTrue(k in list(self.test_obj2.__dict__.keys()))
+        for test_object in [self.test_obj1, self.test_obj2]:
+            self.assertTrue(isinstance(test_object.to_dict(), dict))
 
-        self.assertTrue('__class__' in list(self.test_obj1.to_dict().keys()))
-        self.assertTrue('__class__' in list(self.test_obj2.to_dict().keys()))
-        self.assertEqual(
-                BaseModel.__name__,
-                self.test_obj1.to_dict()['__class__']
-                )
-        self.assertEqual(
-                BaseModel.__name__,
-                self.test_obj2.to_dict()['__class__']
-                )
+            for k in list(test_object.to_dict().keys()):
+                if k != cls:
+                    self.assertTrue(k in list(test_object.__dict__.keys()))
 
-    def test_to_dict_dates(self):
-        """Tests the dates in instance
-        """
-        dt_pattern = r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.?\d*'
-        self.assertTrue(isinstance(
-            self.test_obj1.to_dict()['created_at'],
-            str
-            ))
-        self.assertEqual(
-                    re.findall(
-                            dt_pattern,
-                            self.test_obj1.to_dict()['created_at']
-                        ),
-                    [self.test_obj1.to_dict()['created_at']]
-                )
-        self.assertTrue(isinstance(
-            self.test_obj1.to_dict()['updated_at'],
-            str
-            ))
-        self.assertEqual(
-                    re.findall(
-                            dt_pattern,
-                            self.test_obj1.to_dict()['updated_at']
-                        ),
-                    [self.test_obj1.to_dict()['updated_at']]
-                )
-        self.assertTrue(isinstance(
-            self.test_obj2.to_dict()['created_at'],
-            str
-            ))
-        self.assertEqual(
-                    re.findall(
-                            dt_pattern,
-                            self.test_obj2.to_dict()['created_at']
-                        ),
-                    [self.test_obj2.to_dict()['created_at']]
-                )
-        self.assertTrue(isinstance(
-            self.test_obj2.to_dict()['updated_at'],
-            str
-            ))
-        self.assertEqual(
-                    re.findall(
-                            dt_pattern,
-                            self.test_obj2.to_dict()['updated_at']
-                        ),
-                    [self.test_obj2.to_dict()['updated_at']]
-                )
+            self.assertTrue(cls in list(test_object.to_dict().keys()))
+            self.assertEqual(BaseModel.__name__, test_object.to_dict()[cls])
+
+            for i in ['created_at', 'updated_at']:
+                self.assertTrue(isinstance(test_object.to_dict()[i], str))
+                self.assertTrue(iso_pattern.match(test_object.to_dict()[i]))
 
 
 if __name__ == '__main__':
