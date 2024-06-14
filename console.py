1fcce691443f1613b77babca9db7c4bf6a10319c
Amro Sati <amrosatti98@gmail.com>
2024-06-14 21:52:37 +0300

    refactored comments in console

--- console.py
+++ console.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-""" A command interpter """
+"""Defines a command interpter
+"""
+
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
-    """ Command interpeter from from the cmd class
+    """Command interpeter from from the cmd class
     """
+
     prompt = '(hbnb)'
 
     def do_quit(self, line):
-        """ Quit command to exit the program """
+        """Quit command to exit the program
+        """
         return True
 
     def do_EOF(self, line):
-        """ Exit the program """
+        """Exit the program
+        """
         return True
 
     def emptyline(self):
         pass
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
